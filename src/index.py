9cb57c86a986bf0f7a3f4f14cfc19f2df12e3d66
Joona Nikula <nikulajoona@gmail.com>
2023-11-09 16:33:08 +0200

    tehtävä 3 valmis

--- src/index.py
+++ src/index.py
@@ -5,15 +5,27 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    luonnin_jalkeen(mehua=mehua, olutta=olutta)
+    olut_jotain(olutta=olutta)
+    mehu_setterit(mehua=mehua)
+    virhetilanteita()
+    lisaa_olutta(olutta=olutta)
+    lisaa_mehua(mehua=mehua)
+    ota_olutta(olutta=olutta)
+    ota_mehua(mehua=mehua)
+
+def luonnin_jalkeen(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def olut_jotain(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +34,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def virhetilanteita():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,22 +44,26 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def lisaa_olutta(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def lisaa_mehua(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def ota_olutta(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def ota_mehua(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
